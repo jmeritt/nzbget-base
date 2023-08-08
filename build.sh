dfff510d6abd2f0fc264361626e063b4add631a5
jmeritt <jaime.meritt@gmail.com>
2023-08-08 17:13:41 -0400

    jammy update

--- build.sh
+++ build.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
-docker build -t jmeritt/nzbget .
+docker buildx build --platform linux/amd64 --no-cache -t jmeritt/nzbget .
 docker push jmeritt/nzbget
+kubectl rollout restart deployment nzbget
